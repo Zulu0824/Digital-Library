5334ad3abecb4d50d01ff921f2ca961e702f58a6
Zulu0824 <sriharsha.patta@gmail.com>
2026-07-11 09:14:13 +0530

    Add: Constructor function for a book in JS

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ addBook.addEventListener("click", () => {
 
 canceBook.addEventListener("click", () => {
     dialog.close();
-})
+});
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addNewBook(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
